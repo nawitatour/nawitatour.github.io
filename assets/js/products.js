e921a422bb7d8cb543262f8c45a4ceab0fe81095
nawitatour <nawitainternasionalberkah@gmail.com>
2024-12-02 15:12:51 +0700

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -9,28 +9,28 @@ const products = [
   },
   {
     image: "assets/img/product2.webp",
-    label: "NEW!",
+    label: "HOT!",
     title: "Nawita - Hunting Aurora 2025",
     price: "Rp.38,99 JT",
     link: "https://wa.me/6282311883534?text=Hallo%20admin,%20Saya%20ingin%20bertanya%20mengenai%20Winter%20Camp%202025.%20Bagaimana%20ya%3F",
   },
   {
     image: "assets/img/product3.webp",
-    label: "NEW!",
+    label: "",
     title: "Nawita - Suvenir Umroh",
     price: "Rp.1 JT",
     link: "https://wa.me/6282311883534?text=Hallo%20admin,%20Saya%20ingin%20bertanya%20mengenai%20Winter%20Camp%202025.%20Bagaimana%20ya%3F",
   },
   {
     image: "assets/img/product4.webp",
-    label: "NEW!",
+    label: "",
     title: "Nawita - Suvenir Umroh",
     price: "Rp.1 JT",
     link: "https://wa.me/6282311883534?text=Hallo%20admin,%20Saya%20ingin%20bertanya%20mengenai%20Winter%20Camp%202025.%20Bagaimana%20ya%3F",
   },
   {
     image: "assets/img/product5.webp",
-    label: "NEW!",
+    label: "",
     title: "Nawita - Pemesanan Hotel",
     price: "Rp.1 JT",
     link: "https://wa.me/6282311883534?text=Hallo%20admin,%20Saya%20ingin%20bertanya%20mengenai%20Winter%20Camp%202025.%20Bagaimana%20ya%3F",
@@ -50,6 +50,7 @@ products.forEach((product) => {
     `
       <div class="product__circle"></div>
       <img src="${product.image}" alt="" class="product__img">
+      <h3 class="product__title blinking-text">${product.label}</h3>
       <h3 class="product__title">${product.title}</h3>
       <span class="product__price">${product.price}</span>
       <button class="button--flex product__button" onclick="window.location.href='${product.link}'">
