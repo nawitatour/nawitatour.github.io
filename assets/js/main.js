f0eedf4ed8b132344083c3306931f88e1fdfef13
nawitatour <nawitainternasionalberkah@gmail.com>
2024-12-03 13:10:46 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,128 @@
-// Data produk
-const products = [
-  {
-    image: "assets/img/product1.webp",
-    label: "HOT!",
-    title: "Nawita - Summer School 2025",
-    price: "Rp.2,9 JT",
-    link: "https://wa.me/6282311883534?text=Hallo%20admin,%20Saya%20ingin%20bertanya%20mengenai%20regulasi%20dari%20Summer%20School%202025.%20Bagaimana%20ya%3F",
-  },
-  {
-    image: "assets/img/product2.webp",
-    label: "HOT!",
-    title: "Nawita - Hunting Aurora 2025",
-    price: "Rp.38,99 JT",
-    link: "https://wa.me/6282311883534?text=Hallo%20admin,%20Saya%20ingin%20bertanya%20mengenai%20Winter%20Camp%202025.%20Bagaimana%20ya%3F",
-  },
-  {
-    image: "assets/img/product3.webp",
-    label: "",
-    title: "Nawita - Suvenir Umroh",
-    price: "Rp.1 JT",
-    link: "https://wa.me/6282311883534?text=Hallo%20admin,%20Saya%20ingin%20bertanya%20mengenai%20Winter%20Camp%202025.%20Bagaimana%20ya%3F",
-  },
-  {
-    image: "assets/img/product4.webp",
-    label: "",
-    title: "Nawita - Suvenir Umroh",
-    price: "Rp.1 JT",
-    link: "https://wa.me/6282311883534?text=Hallo%20admin,%20Saya%20ingin%20bertanya%20mengenai%20Winter%20Camp%202025.%20Bagaimana%20ya%3F",
-  },
-];
+const navMenu = document.getElementById('nav-menu'),
+	navToggle = document.getElementById('nav-toggle'),
+	navClose = document.getElementById('nav-close')
 
-// Kontainer elemen
-const productContainer = document.querySelector(".product__container");
+if (navToggle) {
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show-menu')
+	})
+}
 
-// Loop untuk membuat elemen card
-products.forEach((product) => {
-  const productCard = document.createElement("article");
-  productCard.className = "product__card";
+if (navClose) {
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show-menu')
+	})
+}
 
-  productCard.innerHTML = 
-    `
-      <div class="product__circle"></div>
-      <img src="${product.image}" alt="" class="product__img">
-      <h3 class="product__title blinking-text">${product.label}</h3>
-      <h3 class="product__title">${product.title}</h3>
-      <span class="product__price">${product.price}</span>
-      <button class="button--flex product__button" onclick="window.location.href='${product.link}'">
-        <i class="ri-whatsapp-line"></i>
-      </button>
-    `;
+const navLink = document.querySelectorAll('.nav__link')
 
-  productContainer.appendChild(productCard);
-});
+function linkAction() {
+	const navMenu = document.getElementById('nav-menu')
+	navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+function scrollHeader() {
+	const header = document.getElementById('header')
+	if (this.scrollY >= 80) header.classList.add('scroll-header');
+	else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+const accordionItems = document.querySelectorAll('.questions__item')
+
+accordionItems.forEach((item) => {
+	const accordionHeader = item.querySelector('.questions__header')
+
+	accordionHeader.addEventListener('click', () => {
+		const openItem = document.querySelector('.accordion-open')
+
+		toggleItem(item)
+
+		if (openItem && openItem !== item) {
+			toggleItem(openItem)
+		}
+	})
+})
+
+const toggleItem = (item) => {
+	const accordionContent = item.querySelector('.questions__content')
+
+	if (item.classList.contains('accordion-open')) {
+		accordionContent.removeAttribute('style')
+		item.classList.remove('accordion-open')
+	} else {
+		accordionContent.style.height = accordionContent.scrollHeight + 'px'
+		item.classList.add('accordion-open')
+	}
+
+}
+
+const sections = document.querySelectorAll('section[Id]')
+
+function scrollActive() {
+	const scrollY = window.pageYOffset;
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 58;
+		const sectionId = current.getAttribute('id');
+		const link = document.querySelector(`.nav__menu a[href="#${sectionId}"]`);
+		document.querySelectorAll('.nav__menu .nav__link').forEach(el => el.classList.remove('active-link'));
+		if (link && scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			link.classList.add('active-link');
+		}
+	});
+}
+
+function scrollUp() {
+	const scrollUp = document.getElementById('scroll-up');
+	if (this.scrollY >= 400) scrollUp.classList.add('show-scroll');
+	else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if (selectedTheme) {
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+	themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+	document.body.classList.toggle(darkTheme)
+	themeButton.classList.toggle(iconTheme)
+	localStorage.setItem('selected-theme', getCurrentTheme())
+	localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+const sr = ScrollReveal({
+	origin: 'top',
+	distance: '60px',
+	duration: 2500,
+	delay: 400,
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__img`, {
+	delay: 500
+})
+sr.reveal(`.home__social`, {
+	delay: 600
+})
+sr.reveal(`.about__img, .contact__box`, {
+	origin: 'left'
+})
+sr.reveal(`.about__data, .contact__form`, {
+	origin: 'right'
+})
+sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, {
+	interval: 100
+})
