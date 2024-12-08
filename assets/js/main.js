a2cde919edb8093d2c78cbf8b880de1d58dff997
nawitatour <nawitainternasionalberkah@gmail.com>
2024-12-08 08:36:17 +0700

    feat: some fcking stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,224 +1,233 @@
-  window.onload = function() {
-  	var modal = document.getElementById("promoModal");
-  	var span = document.getElementById("closeModal");
+window.onload = function() {
+	var modal = document.getElementById("promoModal");
+	var span = document.getElementById("closeModal");
 
-  	modal.style.display = "block";
+	modal.style.visibility = "hidden";
+	modal.style.opacity = "0";
 
-  	span.onclick = function() {
-  		modal.style.display = "none";
-  	}
+	setTimeout(function() {
+		modal.style.visibility = "visible";
+		modal.style.opacity = "1";
+		modal.style.transition = "opacity 0.3s ease";
+	}, 5000);
 
-  	window.onclick = function(event) {
-  		if (event.target == modal) {
-  			modal.style.display = "none";
-  		}
-  	}
-  }
+	span.onclick = function() {
+		modal.style.visibility = "hidden";
+		modal.style.opacity = "0";
+	}
 
-  const navMenu = document.getElementById('nav-menu'),
-  	navToggle = document.getElementById('nav-toggle'),
-  	navClose = document.getElementById('nav-close')
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.visibility = "hidden";
+			modal.style.opacity = "0";
+		}
+	}
+}
 
-  if (navToggle) {
-  	navToggle.addEventListener('click', () => {
-  		navMenu.classList.add('show-menu')
-  	})
-  }
+const navMenu = document.getElementById('nav-menu'),
+	navToggle = document.getElementById('nav-toggle'),
+	navClose = document.getElementById('nav-close')
 
-  if (navClose) {
-  	navClose.addEventListener('click', () => {
-  		navMenu.classList.remove('show-menu')
-  	})
-  }
+if (navToggle) {
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show-menu')
+	})
+}
 
-  const navLink = document.querySelectorAll('.nav__link')
+if (navClose) {
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show-menu')
+	})
+}
 
-  function linkAction() {
-  	const navMenu = document.getElementById('nav-menu')
-  	navMenu.classList.remove('show-menu')
-  }
-  navLink.forEach(n => n.addEventListener('click', linkAction))
+const navLink = document.querySelectorAll('.nav__link')
 
-  function scrollHeader() {
-  	const header = document.getElementById('header')
-  	if (this.scrollY >= 80) header.classList.add('scroll-header');
-  	else header.classList.remove('scroll-header')
-  }
-  window.addEventListener('scroll', scrollHeader)
+function linkAction() {
+	const navMenu = document.getElementById('nav-menu')
+	navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
-  const accordionItems = document.querySelectorAll('.questions__item')
+function scrollHeader() {
+	const header = document.getElementById('header')
+	if (this.scrollY >= 80) header.classList.add('scroll-header');
+	else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
-  accordionItems.forEach((item) => {
-  	const accordionHeader = item.querySelector('.questions__header')
+const accordionItems = document.querySelectorAll('.questions__item')
 
-  	accordionHeader.addEventListener('click', () => {
-  		const openItem = document.querySelector('.accordion-open')
+accordionItems.forEach((item) => {
+	const accordionHeader = item.querySelector('.questions__header')
 
-  		toggleItem(item)
+	accordionHeader.addEventListener('click', () => {
+		const openItem = document.querySelector('.accordion-open')
 
-  		if (openItem && openItem !== item) {
-  			toggleItem(openItem)
-  		}
-  	})
-  })
+		toggleItem(item)
 
-  const toggleItem = (item) => {
-  	const accordionContent = item.querySelector('.questions__content')
+		if (openItem && openItem !== item) {
+			toggleItem(openItem)
+		}
+	})
+})
 
-  	if (item.classList.contains('accordion-open')) {
-  		accordionContent.removeAttribute('style')
-  		item.classList.remove('accordion-open')
-  	} else {
-  		accordionContent.style.height = accordionContent.scrollHeight + 'px'
-  		item.classList.add('accordion-open')
-  	}
+const toggleItem = (item) => {
+	const accordionContent = item.querySelector('.questions__content')
 
-  }
+	if (item.classList.contains('accordion-open')) {
+		accordionContent.removeAttribute('style')
+		item.classList.remove('accordion-open')
+	} else {
+		accordionContent.style.height = accordionContent.scrollHeight + 'px'
+		item.classList.add('accordion-open')
+	}
 
-  const sections = document.querySelectorAll('section[Id]')
+}
 
-  function scrollActive() {
-  	const scrollY = window.pageYOffset;
-  	sections.forEach(current => {
-  		const sectionHeight = current.offsetHeight;
-  		const sectionTop = current.offsetTop - 58;
-  		const sectionId = current.getAttribute('id');
-  		const link = document.querySelector(`.nav__menu a[href="#${sectionId}"]`);
-  		document.querySelectorAll('.nav__menu .nav__link').forEach(el => el.classList.remove('active-link'));
-  		if (link && scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-  			link.classList.add('active-link');
-  		}
-  	});
-  }
+const sections = document.querySelectorAll('section[Id]')
 
-  function scrollUp() {
-  	const scrollUp = document.getElementById('scroll-up');
-  	if (this.scrollY >= 400) scrollUp.classList.add('show-scroll');
-  	else scrollUp.classList.remove('show-scroll')
-  }
-  window.addEventListener('scroll', scrollUp)
+function scrollActive() {
+	const scrollY = window.pageYOffset;
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 58;
+		const sectionId = current.getAttribute('id');
+		const link = document.querySelector(`.nav__menu a[href="#${sectionId}"]`);
+		document.querySelectorAll('.nav__menu .nav__link').forEach(el => el.classList.remove('active-link'));
+		if (link && scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			link.classList.add('active-link');
+		}
+	});
+}
 
-  const themeButton = document.getElementById('theme-button')
-  const darkTheme = 'dark-theme'
-  const iconTheme = 'ri-sun-line'
+function scrollUp() {
+	const scrollUp = document.getElementById('scroll-up');
+	if (this.scrollY >= 400) scrollUp.classList.add('show-scroll');
+	else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
-  const selectedTheme = localStorage.getItem('selected-theme')
-  const selectedIcon = localStorage.getItem('selected-icon')
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
 
-  const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-  const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
 
-  if (selectedTheme) {
-  	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  	themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
-  }
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
 
-  themeButton.addEventListener('click', () => {
-  	document.body.classList.toggle(darkTheme)
-  	themeButton.classList.toggle(iconTheme)
-  	localStorage.setItem('selected-theme', getCurrentTheme())
-  	localStorage.setItem('selected-icon', getCurrentIcon())
-  })
+if (selectedTheme) {
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+	themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
 
-  const sr = ScrollReveal({
-  	origin: 'top',
-  	distance: '60px',
-  	duration: 2500,
-  	delay: 400,
-  })
+themeButton.addEventListener('click', () => {
+	document.body.classList.toggle(darkTheme)
+	themeButton.classList.toggle(iconTheme)
+	localStorage.setItem('selected-theme', getCurrentTheme())
+	localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
-  sr.reveal(`.home__data`)
-  sr.reveal(`.home__img`, {
-  	delay: 500
-  })
-  sr.reveal(`.home__social`, {
-  	delay: 600
-  })
-  sr.reveal(`.about__img, .contact__box`, {
-  	origin: 'left'
-  })
-  sr.reveal(`.about__data, .contact__form`, {
-  	origin: 'right'
-  })
-  sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, {
-  	interval: 100
-  })
+const sr = ScrollReveal({
+	origin: 'top',
+	distance: '60px',
+	duration: 2500,
+	delay: 400,
+})
 
-  const carousel = document.querySelector(".carousel");
-  const backgroundImage = document.querySelector(".bg-image");
+sr.reveal(`.home__data`)
+sr.reveal(`.home__img`, {
+	delay: 500
+})
+sr.reveal(`.home__social`, {
+	delay: 600
+})
+sr.reveal(`.about__img, .contact__box`, {
+	origin: 'left'
+})
+sr.reveal(`.about__data, .contact__form`, {
+	origin: 'right'
+})
+sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, {
+	interval: 100
+})
 
-  const leftArrow = document.querySelector(".left-arrow");
-  const rightArrow = document.querySelector(".right-arrow");
+const carousel = document.querySelector(".carousel");
+const backgroundImage = document.querySelector(".bg-image");
 
-  let currentIndex = 0;
-  let prevIndex;
-  const images = document.querySelectorAll(".carousel-image");
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
 
-  const totalImages = Object.keys(images).length;
+let currentIndex = 0;
+let prevIndex;
+const images = document.querySelectorAll(".carousel-image");
 
-  const imageWidth = 520;
-  console.log("getbounding1", images[1].getBoundingClientRect());
+const totalImages = Object.keys(images).length;
 
-  leftArrow.addEventListener("click", () => {
-  	prevIndex = currentIndex;
-  	currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-  	carousel.style.transform = `translateX(-${imageWidth}px)`;
-  	carousel.insertBefore(images[currentIndex], carousel.firstChild);
+const imageWidth = 520;
+console.log("getbounding1", images[1].getBoundingClientRect());
 
-  	setTimeout(() => {
-  		carousel.style.transform = "";
-  		carousel.classList.add("sliding-transition");
-  	}, 10);
+leftArrow.addEventListener("click", () => {
+	prevIndex = currentIndex;
+	currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+	carousel.style.transform = `translateX(-${imageWidth}px)`;
+	carousel.insertBefore(images[currentIndex], carousel.firstChild);
 
-  	setTimeout(() => {
-  		carousel.classList.remove("sliding-transition");
-  	}, 490);
-  });
+	setTimeout(() => {
+		carousel.style.transform = "";
+		carousel.classList.add("sliding-transition");
+	}, 10);
 
-  rightArrow.addEventListener("click", () => {
-  	carousel.classList.add("sliding-transition");
+	setTimeout(() => {
+		carousel.classList.remove("sliding-transition");
+	}, 490);
+});
 
-  	prevIndex = currentIndex;
-  	currentIndex = (currentIndex + 1) % totalImages;
+rightArrow.addEventListener("click", () => {
+	carousel.classList.add("sliding-transition");
 
-  	carousel.style.transform = `translateX(-${imageWidth}px)`;
+	prevIndex = currentIndex;
+	currentIndex = (currentIndex + 1) % totalImages;
 
-  	setTimeout(() => {
-  		carousel.appendChild(images[prevIndex]);
-  		carousel.classList.remove("sliding-transition");
-  		carousel.style.transform = "";
-  	}, 500);
-  });
+	carousel.style.transform = `translateX(-${imageWidth}px)`;
+
+	setTimeout(() => {
+		carousel.appendChild(images[prevIndex]);
+		carousel.classList.remove("sliding-transition");
+		carousel.style.transform = "";
+	}, 500);
+});
 
 function zoomImage(image) {
-  const zoomedImageContainer = document.getElementById('zoomedImageContainer');
-  const zoomedImage = document.getElementById('zoomedImage');
+	const zoomedImageContainer = document.getElementById('zoomedImageContainer');
+	const zoomedImage = document.getElementById('zoomedImage');
 
-  zoomedImage.src = image.src; 
-  zoomedImageContainer.style.display = 'flex'; 
+	zoomedImage.src = image.src;
+	zoomedImageContainer.style.display = 'flex';
 }
 
 function closeZoom() {
-  const zoomedImageContainer = document.getElementById('zoomedImageContainer');
-  zoomedImageContainer.style.display = 'none'; 
+	const zoomedImageContainer = document.getElementById('zoomedImageContainer');
+	zoomedImageContainer.style.display = 'none';
 }
 
 const imageSlider = [
-  'assets/img/header.webp',
-  'assets/img/header2.webp',
+	'assets/img/header.webp',
+	'assets/img/header2.webp',
 ];
 
 let currentImage = 0;
 
 function changeImage() {
-  const slideshow = document.getElementById('slideshow');
-  
-  slideshow.classList.add('fade-out');
-  setTimeout(() => {
-    currentImage = (currentImage + 1) % imageSlider.length;
-    slideshow.src = imageSlider[currentImage];
-    slideshow.classList.remove('fade-out');
-  }, 1000); 
+	const slideshow = document.getElementById('slideshow');
+
+	slideshow.classList.add('fade-out');
+	setTimeout(() => {
+		currentImage = (currentImage + 1) % imageSlider.length;
+		slideshow.src = imageSlider[currentImage];
+		slideshow.classList.remove('fade-out');
+	}, 1000);
 
 }
 
